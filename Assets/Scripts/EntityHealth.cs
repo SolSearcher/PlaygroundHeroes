c7512e09d0f17f3c3567f09f48618acd86a91ac7
Reed Scriven <r433scriven@gmail.com>
2018-10-21 12:41:56 -0700

    Midst of Mark Merge
    Merging the code Mark did with the proejct. Added in the health bars so far and I'm working on a generic health system.

---
+++ Assets/Scripts/EntityHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // void Update () 
+    // {
+    // 	// if(damaged)
+    // 	// {
+    // 	// 	damageImage.color = flashColor;
+    // 	// }
+    // 	// else
+    // 	// {
+    // 	// 	damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+    // 	// }
+    // 	// damaged = false;
+
+    // }
+
+    public void TakeDamage(int amount)
+    {
+        //damaged = true;
+        Debug.Log(currentHealth);
+        currentHealth -= amount;
+
+        // Death Check
+        if (currentHealth < 0)
+        {
+            print("#ded");
+        }
+    }
+}
