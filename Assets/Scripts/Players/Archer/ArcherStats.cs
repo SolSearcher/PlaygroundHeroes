73f94af42833dfd5fc3b54f4ca48cf63ceaf6838
AlexStanke <39075450+AlexStanke@users.noreply.github.com>
2018-10-25 18:15:49 -0700

    Changed player stat code
    changed health bars and way the health and stanima are accessed
    now must use KnightStats.health or ArcherStats.energy

---
+++ Assets/Scripts/Players/Archer/ArcherStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherStats : MonoBehaviour
+{
+
+    [SerializeField]
+    private Stat health;
+    [SerializeField]
+    private Stat energy;
+
+    private void Awake()
+    {
+        health.Initialize();
+        energy.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Players/Knight/KnightStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightStats : MonoBehaviour {
+
+    [SerializeField]
+    private Stat health;
+    [SerializeField]
+    private Stat energy;
+
+    private void Awake()
+    {
+        health.Initialize();
+        energy.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
